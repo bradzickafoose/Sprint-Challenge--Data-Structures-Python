689c1bea50454a41dc6ff9a345b02475332f1d5e
Brad <brad@viziworx.com>
2020-03-27 12:26:42 -0400

    adds ring buffer data structure implementation with append and get methods

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,41 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # If the list is empty, add item to the head and the current becomes the head
+        if len(self.storage) == 0:
+          self.storage.add_to_head(item)
+          self.current = self.storage.head
+          return
+
+        # If the list is at capacity, remove the oldest (current.next)
+        if len(self.storage) == self.capacity:
+
+          # If the current is the tail, overwrite the head
+          if self.current == self.storage.tail:
+            self.storage.head.value = item
+            self.current = self.storage.head
+
+          # Else, if the current.next exists, overwrite the current.next
+          else:
+            self.current.next.value = item
+            self.current = self.current.next
+
+        #Else, if the storage is not at capacity, add value to the tail
+        else:
+          self.storage.add_to_tail(item)
+          self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+        cursor = self.storage.head
 
-        # TODO: Your code here
+        if cursor == None:
+          return list_buffer_contents
+
+        while cursor:
+          list_buffer_contents.append(cursor.value)
+          cursor = cursor.next
 
         return list_buffer_contents
 
