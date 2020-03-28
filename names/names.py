62373bcb5bf896e8b8bcfe1e08c2eb2aefa23f67
Brad <brad@viziworx.com>
2020-03-28 13:06:20 -0400

    adds a binary search tree to test runtime optimization

--- names/names.py
+++ names/names.py
@@ -1,5 +1,6 @@
 import time
 from lru_cache import LRUCache
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -19,20 +20,34 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
+bst = BinarySearchTree(names_1[0])
+
+for name_1 in names_1:
+  bst.insert(name_1)
+
+for name_2 in names_2:
+  if bst.contains(name_2):
+    duplicates.append(name_2)
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"Binary Search Tree runtime: {end_time - start_time} seconds")
+
+# ---------- Stretch Goal -----------
+# Python has built-in tools that allow for a very efficient approach to this problem
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
+
 lru_cache = LRUCache(10000)
+lru_duplicates = []
 
 for name_1 in names_1:
   lru_cache.set(name_1, name_1)
 
 for name_2 in names_2:
   if lru_cache.get(name_2):
-    duplicates.append(name_2)
+    lru_duplicates.append(name_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
-# ---------- Stretch Goal -----------
-# Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
-# structures, but you may not import any additional libraries that you did not write yourself.
+print (f"{len(lru_duplicates)} duplicates:\n\n{', '.join(lru_duplicates)}\n\n")
+print (f"LRU cache runtime: {end_time - start_time} seconds")
